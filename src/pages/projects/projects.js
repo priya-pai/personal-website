2623ccea30c505fca7ad3591479ac0ce6931a520
priya-pai <pbp2110@columbia.edu>
2020-07-26 19:57:56 -0500

    added projects to page

--- src/pages/projects/projects.js
+++ src/pages/projects/projects.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import {Grid} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import ccs from './ccs.png';
+import mobileportal from './mobileportal.png';
+import racial from './racial.png';
+import quarto from './quarto.png';
+import adobe from './adobe.png';
+import cuimpact from './cuimpact.png';
+import evictions from './evictions.png';
+import './projects.css';
 import Paper from '@material-ui/core/Paper';
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -11,24 +20,69 @@ const useStyles = makeStyles((theme) => ({
         display: 'inline-block',
         fontSize:'16px',
         lineHeight: '1.4',
-        fontWeight: '500'
+        fontWeight: '500',
+        width:'1050px'
+    },
+    project:{
+      border: '2px solid #B2CC98'
     }
 }));
 
+const projectsData = [
+  {
+    title:'UChicago Poverty Lab - Chicago College Success',
+    image: ccs,
+    link:'/projects/ccs'
+  },
+  {
+    title:'Columbia - Mobile Library Portal',
+    image: mobileportal,
+    link:'https://group1-pit.github.io/'
+  },
+  {
+    title:'ACLU - Racial Disparity in Marijuana Arrests',
+    image: racial,
+    link:'/projects/aclu-arrests'
+  },
+  {
+    title:'ACLU - Evictions Analysis',
+    image: evictions,
+    link:'/projects/evictions'
+  },
+  {
+    title:'Adobe Research - Mobile Deep Learning Narratives',
+    image: adobe,
+    link:'/projects/adobe'
+  },
+  {
+    title:'Quarto Magazine Virtual Launch',
+    image: quarto,
+    link:'/projects/quarto'
+  },
+  {
+    title:'Columbia - CUImpact',
+    image: cuimpact,
+    link:'/projects/cuimpact'
+  }
+]
+
 export function Projects(){
-    const classes = useStyles();
+    //const classes = useStyles();
     return (
-        <div className={classes.desc}>
+        <div className="desc">
         <h1> projects </h1>
-        <Grid container spacing={1}>
-        <Grid container item xs={12} spacing={3}>
-           
-        </Grid>
-        <Grid container item xs={12} spacing={3}>
-        </Grid>
-        <Grid container item xs={12} spacing={3}>
-            
-        </Grid>
+        <Grid container spacing={4}>
+          {
+            [...Array(projectsData.length).keys()].map((index) =>
+            <Grid container item xs={4} md={4} lg={4} key ={projectsData[index].title}>
+              <div width='300px'height='300px' className="projectDiv">
+                 <a href={projectsData[index].link}><img className="project"
+                  src={projectsData[index].image} alt={projectsData[index].title} width='300px'height='300px'/>
+                  </a> 
+              </div>
+              </Grid>)
+          }
+
         </Grid>
         </div>
     )
