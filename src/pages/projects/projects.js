ec00c5465201f52a2d6edc8664ed2eeef1b811f2
priya-pai <pbp2110@columbia.edu>
2020-07-28 13:17:55 -0500

    took out targets

--- src/pages/projects/projects.js
+++ src/pages/projects/projects.js
@@ -65,7 +65,6 @@ const projectsData = [
   }
 ]
 
-
 export function Projects(){
 
     return (
@@ -76,13 +75,13 @@ export function Projects(){
             [...Array(projectsData.length).keys()].map((index) =>
             <Grid container item xs={4} md={4} lg={4} key ={projectsData[index].title}>
               <div width='300px'height='300px' className="projectDiv">
-                  <Link to={projectsData[index].link} style={{ textDecoration: 'none' }} target="_blank">
+              <Link to={projectsData[index].link} style={{ textDecoration: 'none' }}>
                   <span className="txt">
                     <h3>{projectsData[index].title}</h3>
                   </span>
                  <img className="project" src={projectsData[index].image} 
                  alt={projectsData[index].title} width='300px'height='300px'/>
-                  </Link> 
+                </Link> 
               </div>
               </Grid>)
           }
